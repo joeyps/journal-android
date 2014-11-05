b71d82a8ca89217227661150d721146aceac60b4
joeyps <speshiou@gmail.com>
2014-11-05 21:51:25 +0800

    Initial commit

---
+++ app/src/main/java/joey/thosedays/WelcomeActivity.java
@@ -0,0 +1,91 @@
+package joey.thosedays;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by joey on 14/11/5.
+ */
+public class WelcomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        findViewById(R.id.button_fb_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // start Facebook Login
+                Session.openActiveSession(WelcomeActivity.this, true, new Session.StatusCallback() {
+
+                    // callback when session changes state
+                    @Override
+                    public void call(Session session, SessionState state, Exception exception) {
+                        if (session.isOpened()) {
+
+                            // make request to the /me API
+                            Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                                // callback after Graph API response with user object
+                                @Override
+                                public void onCompleted(GraphUser user, Response response) {
+                                    if (user != null) {
+                                        Toast.makeText(WelcomeActivity.this, "Hello " + user.getName() + "!", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent();
+                                        intent.setClass(WelcomeActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            }).executeAsync();
+                        }
+                    }
+                });
+            }
+        });
+        printKeyHash();
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
+
+    private void printKeyHash() {
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "joey.thosedays",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
